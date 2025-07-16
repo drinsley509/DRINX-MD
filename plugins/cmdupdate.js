149b20496c872ab962dfaac5bd498a7ee8801a1c
zokou <alixexadron7@gmail.com>
2025-07-16 08:22:11 -0400

    Add files via upload

---
+++ plugins/cmdupdate.js
@@ -0,0 +1,69 @@
+/*  ______    ___    __    ___     ___
+   /  __  \  |   |  |__|   \  \   /  /
+  /  /__\  \ |   |   __     \  \ /  /
+  |  ____  | |   |  |  |    /  / \  \
+  |  |  |  | |   |  |  |   /  /   \  \
+  |__|  |__| |___|  |__|  /__/     \__\
+
+
+ANYWAY, YOU MUST GIVE CREDIT TO MY CODE WHEN COPY IT
+CONTACT ME HERE +50935959059
+email;alixexadron7@gmail.com
+*/
+
+const fs = require('fs');
+const path = require('path');
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+cmd({
+  pattern: "updatecmd",
+  react: "🧞",
+  desc: "Update commands.",
+  category: "owner",
+  filename: __filename
+},
+async (conn, mek, m, {
+  from,
+  quoted,
+  body,
+  isCmd,
+  command,
+  args,
+  q,
+  isGroup,
+  sender,
+  senderNumber,
+  botNumber2,
+  botNumber,
+  pushname,
+  isMe,
+  isOwner,
+  groupMetadata,
+  groupName,
+  participants,
+  groupAdmins,
+  isBotAdmins,
+  isAdmins,
+  reply
+}) => {
+  try {
+    if (!isOwner) return reply("Only bot owners can use this command.");
+    
+    const pluginsDir = path.join(__dirname, '../plugins');
+    const files = fs.readdirSync(pluginsDir);
+    
+    for (const file of files) {
+      if (file.endsWith('.js')) {
+        const filePath = path.join(pluginsDir, file);
+        require(filePath);
+        console.log(`Loaded ${file}`);
+      }
+    }
+    
+    reply("Commands updated successfully.");
+  } catch (e) {
+    console.log(e);
+    reply(`Error updating commands: ${e.message}`);
+  }
+});
